066efdc36ac6b87ae00d0c1afffa78355b205fd5
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-22 11:12:13 +0000

    chore: make canary equal prod

--- avm_scripts_canary/docs-gen.sh
+++ avm_scripts_canary/docs-gen.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+generate_docs () {
+  local dir=$1
+  echo "===> Generating examples documentation in $dir"
+  rm -f "$dir/.terraform.lock.hcl"
+  if [ -f ".terraform-docs.yml" ]; then
+    terraform-docs -c ".terraform-docs.yml" "$dir"
+  else
+    terraform-docs markdown table --output-file "$dir/README.md" --output-mode inject "$dir"
+  fi
+}
+
 echo "==> Generating module documentation..."
 rm -f .terraform.lock.hcl
 terraform-docs -c .terraform-docs.yml .
 echo "==> Generating examples documentation..."
-examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
-for d in $examples; do
-  echo "===> Generating examples documentation in " $d
-  cd $d
-  rm -f .terraform.lock.hcl
-  if [ -f "../.terraform-docs.yml" ]; then
-  	terraform-docs -c ../.terraform-docs.yml .
-  else
-  	terraform-docs markdown table --output-file README.md --output-mode inject .
-  fi
-
+if [ ! -d examples ]; then
+  echo "==> Error - no examples directory found"
+  exit 1
+fi
+cd examples
+subexamples=$(find ./ -maxdepth 1 -mindepth 1 -type d)
+for d in $subexamples; do
+  generate_docs $d
 done
+cd ..
